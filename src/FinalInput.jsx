38e6fa5547f38cf8dc9f9af0c03c5b33a75c85a6
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2023-06-02 23:06:42 +0545

    Update FinalInput.jsx

--- src/FinalInput.jsx
+++ src/FinalInput.jsx
@@ -212,7 +212,7 @@ const FinalInput = () => {
                                     <div className=" mt-4 flex justify-center space-x-2">
                                         <button onClick={() => downloadmp3(items.id.videoId)} class="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                                             <span class="px-2 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                                Dowbload Mp3
+                                                Download Mp3
                                             </span>
                                         </button>
                                         <button onClick={() => downloadmp4(items.id.videoId)} class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
